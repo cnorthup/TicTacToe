be45a4ad04d3baff41fe3fcc786dae9e4d4c3137
Charles Northup <cjnorth40@gmail.com>
2014-03-13 17:25:17 -0500

    works but no winner

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -8,7 +8,22 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIGestureRecognizerDelegate>
+{
+    IBOutlet UILabel *myLabelOne;
+    IBOutlet UILabel *myLabelTwo;
+    IBOutlet UILabel *myLabelThree;
+    IBOutlet UILabel *myLabelFour;
+    IBOutlet UILabel *myLabelFive;
+    IBOutlet UILabel *myLabelSix;
+    IBOutlet UILabel *myLabelSeven;
+    IBOutlet UILabel *myLabelEight;
+    IBOutlet UILabel *myLabelNine;
+    IBOutlet UILabel *whichPlayerLabel;
+    CGPoint findLabelTapped;
+    bool whichPlayer;
+    NSMutableArray *labels;
+}
 
 @end
 
@@ -16,14 +31,55 @@
 
 - (void)viewDidLoad
 {
+    labels =[[NSMutableArray alloc] initWithObjects:myLabelOne, myLabelTwo, myLabelThree, myLabelFour, myLabelFive, myLabelSix, myLabelSeven, myLabelEight, myLabelNine, nil];
+    whichPlayerLabel.text = @"Player 1";
+    whichPlayerLabel.backgroundColor = [UIColor redColor];
+    [whichPlayerLabel sizeToFit];
+    whichPlayer = YES;
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+- (void)writeLabel:(UILabel *)label
+{
+    if (whichPlayer == YES) {
+        label.text = @"O";
+        label.backgroundColor = [UIColor redColor];
+    }else{
+        label.text = @"X";
+        label.backgroundColor = [UIColor blueColor];
+    }
+}
+
+
+
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)tapGestureRecongizer
+{
+    UILabel *removeLabel;
+    findLabelTapped = [tapGestureRecongizer locationInView:self.view];
+    for (UILabel* foundLabel in labels) {
+        if (CGRectContainsPoint(foundLabel.frame, findLabelTapped)) {
+            [self writeLabel:foundLabel];
+            removeLabel = foundLabel;
+        }
+    }
+    [labels removeObject:removeLabel];
+    whichPlayer = !whichPlayer;
+    if (whichPlayer) {
+        whichPlayerLabel.text = @"Player2";
+        whichPlayerLabel.backgroundColor = [UIColor blueColor];
+    }
+    else{
+        whichPlayerLabel.text = @"Player1";
+        whichPlayerLabel.backgroundColor = [UIColor redColor];
+    }
+    
+}
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
